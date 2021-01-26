126c058a73d5806ebd97a15265d1297ea04dd267
Andrey04Dev <andrey.marin12net@gmail.com>
2021-01-25 21:28:09 -0600

    updated

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,19 @@
-import { Router } from 'express'
+import e, { Router } from 'express'
 import bcrypt from 'bcryptjs'
 import { tableUsers } from '../../db/db'
+import { check, validationResult } from 'express-validator'
 
 const router =  Router()
-
-router.post('/register', async(req,res)=>{
+const checkArray =[
+    check('username','Username is required').not().isEmpty(),
+    check('email','The email is required').isEmail(),
+    check('password','The password is required').not().isEmpty()
+]
+router.post('/register', checkArray, async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({Errors:errors.array()})
+    }
     req.body.password = bcrypt.hashSync(req.body.password, 10)
     const user = await tableUsers.create(req.body);
     res.json(user)
